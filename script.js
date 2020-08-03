0cabb85eda7eae70e12c89441419842a28a19f82
Zachiah Zook <zakhazook1@gmail.com>
2020-08-03 15:34:11 +0000

    has multiple versions of moving things using keys

--- script.js
+++ script.js
@@ -2,26 +2,80 @@ function setup() {
 	createCanvas(500, 400);
 }
 
-var [xrot, yrot, t] = [350, 200, 1];
+var [xpos, ypos, xspeed, yspeed] = [225, 225, 0, 0];
 
 function draw() {
 	background(225);
 	
-	// thing you move
 	fill(0, 255, 0);
-	ellipse(mouseX, mouseY, 50);
+	rect(xpos, ypos, 50, 50);
 	
-	// big wheel
-	fill(255);
-	ellipse(350, 250, 100);
-	
-	// weird moving line
-	fill(0);
-	strokeCap(ROUND);
-	line(mouseX, mouseY, xrot+50, yrot+45);
-	
-	// make it spin
-	xrot += Math.cos((PI/2) * t) * 8;
-	yrot += Math.sin((PI/2) * t) * 8;
-	t += 0.1;
+	if(xpos >= 0 && xpos + 50 <= 500) xpos += xspeed;
+	if(ypos >= 0 && ypos + 50 <= 500) ypos += yspeed;
 }
+
+function keyPressed() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = -2;
+			break;
+		case 39:
+		case 68:
+			xspeed = 2;
+			break;
+		case 38:
+		case 87:
+			yspeed = -2;
+			break;
+		case 40:
+		case 83:
+			yspeed = 2;
+			break;
+	}
+}
+
+function keyReleased() {
+	switch(keyCode) {
+		case 37:
+		case 65:
+			xspeed = 0;
+			break;
+		case 39:
+		case 68:
+			xspeed = 0;
+			break;
+		case 38:
+		case 87:
+			yspeed = 0;
+			break;
+		case 40:
+		case 83:
+			yspeed = 0;
+			break;
+	}
+}
+
+
+function movepaddles(){
+  paddleLTop = paddleLeftY - paddleH/2, 
+ paddleLB = paddleLeftY + paddleH/2, 
+ paddleRTop = paddleRightY - paddleH/2, 
+ paddleRB = paddleRightY + paddleH/2;
+
+	if (keyIsDown(87) && (paddleLTop > 0)) {
+		paddleLeftY -= paddleVel;
+	} else if (keyIsDown(83) && (paddleLB < canvasH)) {
+		paddleLeftY += paddleVel;
+	}
+
+	if (keyIsDown(79) && (paddleRTop > 0)) {
+		paddleRightY -= paddleVel;
+	} else if (keyIsDown(76) && (paddleRB < canvasH)) {
+		paddleRightY += paddleVel;
+}
+
+paddleLTop = paddleLeftY - paddleH/2, 
+paddleLB   = paddleLeftY + paddleH/2, 
+paddleRTop = paddleRightY - paddleH/2, 
+paddleRB   = paddleRightY + paddleH/2;
